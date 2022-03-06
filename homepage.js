11b031cba5e566600a2d507d1179dc0526b93f3f
Balaji A <93375333+Balaji21121109@users.noreply.github.com>
2022-03-06 19:34:44 +0530

    a

---
+++ homepage.js
@@ -0,0 +1,96 @@
+function fun1(){
+    var x=document.getElementById('imgg1')
+    x.src='https://openlibrary.org/static/images/onboarding/librarian.png';  
+    var y=document.getElementById("imgg2")
+    y.src="https://openlibrary.org/static/images/onboarding/feedback.png"
+
+}
+function funn2(){
+    var x=document.getElementsByClassName("imgg2")
+    x.src="https://openlibrary.org/static/images/onboarding/librarian.png"
+   
+    var y=document.getElementById("imgg3")
+    y.src="https://openlibrary.org/static/images/onboarding/feedback.png"
+}
+function fun3(){
+    var x=document.getElementById("class_1")
+    x.src="https://covers.openlibrary.org/w/id/4437861-M.jpg"
+    var y=document.getElementById("class_2")
+    y.src="https://covers.openlibrary.org/b/olid/OL22490784M-M.jpg"
+    var z=document.getElementById("class_3")
+    z.src="https://covers.openlibrary.org/w/id/1718654-M.jpg"
+    var n=document.getElementById("class_4")
+    n.src="https://covers.openlibrary.org/b/id/12414469-M.jpg"
+}
+function fun4(){
+    var x=document.getElementById("class_1")
+    x.src="https://covers.openlibrary.org/w/id/481167-M.jpg"
+    var y=document.getElementById("class_2")
+    y.src="https://covers.openlibrary.org/b/olid/OL7171901M-M.jpg"
+    var z=document.getElementById("class_3")
+    z.src="https://covers.openlibrary.org/w/id/8215684-M.jpg"
+    var n=document.getElementById("class_4")
+    n.src="https://covers.openlibrary.org/b/olid/OL23278744M-M.jpg"
+
+}
+function fun5(){
+    var x=document.getElementById("fun51")
+    x.src="https://covers.openlibrary.org/w/id/392915-M.jpg"
+    var y=document.getElementById("fun52")
+    y.src="https://covers.openlibrary.org/b/id/10662024-M.jpg"
+    var z=document.getElementById("fun53")
+    z.src="https://covers.openlibrary.org/b/id/10663695-M.jpg"
+    var n=document.getElementById("fun54")
+    n.src="https://covers.openlibrary.org/w/id/6715674-M.jpg"
+}
+function fun6(){
+    var x=document.getElementById("fun54")
+    x.src="https://covers.openlibrary.org/w/id/481167-M.jpg"
+    var y=document.getElementById("fun53")
+    y.src="https://covers.openlibrary.org/b/olid/OL7171901M-M.jpg"
+    var z=document.getElementById("fun52")
+    z.src="https://covers.openlibrary.org/w/id/8215684-M.jpg"
+    var n=document.getElementById("fun51")
+    n.src="https://covers.openlibrary.org/b/olid/OL23278744M-M.jpg"
+
+}
+function fun7(){
+    var x=document.getElementById("fun71")
+    x.src="https://covers.openlibrary.org/w/id/9000067-M.jpg"
+    var y=document.getElementById("fun72")
+    y.src="https://covers.openlibrary.org/w/id/602202-M.jpg"
+    var z=document.getElementById("fun73")
+    z.src="https://covers.openlibrary.org/w/id/7990188-M.jpg"
+    var n=document.getElementById("fun74")
+    n.src="https://covers.openlibrary.org/w/id/602202-M.jpg"
+}
+function fun8(){
+    var x=document.getElementById("fun71")
+    x.src="https://covers.openlibrary.org/w/id/481167-M.jpg"
+    var y=document.getElementById("fun72")
+    y.src="https://covers.openlibrary.org/w/id/481167-M.jpg"
+    var z=document.getElementById("fun73")
+    z.src="https://covers.openlibrary.org/w/id/6715674-M.jpg"
+    var n=document.getElementById("fun74")
+    n.src="https://covers.openlibrary.org/w/id/7990188-M.jpg"
+}
+function fun9(){
+    var x=document.getElementById("fun91")
+    x.src="https://covers.openlibrary.org/w/id/481167-M.jpg"
+    var y=document.getElementById("fun92")
+    y.src="https://covers.openlibrary.org/w/id/4437861-M.jpg"
+    var z=document.getElementById("fun93")
+    z.src="https://covers.openlibrary.org/w/id/6715674-M.jpg"
+    var n=document.getElementById("fun94")
+    n.src="https://covers.openlibrary.org/w/id/7990188-M.jpg"
+}
+function fun10(){
+    var x=document.getElementById("fun91")
+    x.src="https://covers.openlibrary.org/w/id/481167-M.jpg"
+    var y=document.getElementById("fun92")
+    y.src="https://covers.openlibrary.org/w/id/4437861-M.jpg"
+    var z=document.getElementById("fun93")
+    z.src="https://covers.openlibrary.org/w/id/6715674-M.jpg"
+    var n=document.getElementById("fun94")
+    n.src="https://covers.openlibrary.org/w/id/7990188-M.jpg"
+}
